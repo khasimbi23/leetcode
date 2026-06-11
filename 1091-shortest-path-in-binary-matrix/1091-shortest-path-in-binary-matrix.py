c5c32b41fee0d2c5da0445c7d34e00cace0c7c31
khasimbi23 <shaikkhasimbi223@gmail.com>
2026-06-11 08:50:47 +0530

    Time: 124 ms (80.59%), Space: 19.6 MB (91.92%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,54 +1,20 @@
-from typing import List
-
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        n = len(grid)
-        m = len(grid[0])   # FIX: define m
-        
-        if grid[0][0] == 1 or grid[n-1][m-1] == 1:
+        n=len(grid)
+        if grid[0][0]==1 or grid[n-1][n-1]==1:
             return -1
-        
-        start = [0, 0]
-        
-        visit = []
-        for i in range(n):
-            visit.append([False] * m)   # FIX: m defined
-        
-        q = []
-        q.append(start)
-        visit[0][0] = True
-        
-        level = 0
-        
-        while q:
-            l = len(q)
-            level += 1
-            
-            for k in range(l):
-                t = q.pop(0)
-                i, j = t[0], t[1]
-                
-                # FIX: correct destination condition
-                if i == n-1 and j == m-1:
-                    return level
-                
-                # FIX: correct children list
-                child = [
-                    [i+1, j], [i-1, j],
-                    [i, j+1], [i, j-1],
-                    [i+1, j+1], [i+1, j-1],
-                    [i-1, j+1], [i-1, j-1]
-                ]
-                
-                for c in child:
-                    ci, cj = c[0], c[1]
-                    
-                    if (ci >= 0 and ci < n and 
-                        cj >= 0 and cj < m and 
-                        not visit[ci][cj] and 
-                        grid[ci][cj] == 0):
-                        
-                        visit[ci][cj] = True
-                        q.append([ci, cj])
-        
+        d=deque([[0,0,1]])
+        directions=[[1,0],[-1,0],[0,-1],[0,1],[1,1],[-1,-1],[1,-1],[-1,1]]
+        while d:
+            r,c,path=d.popleft()
+            if r==n-1 and c==n-1:
+                return path
+            for x,y in directions:
+                nr,nc=r+x,y+c
+                if 0<=nr<n and 0<=nc<n and grid[nr][nc]==0:
+                    d.append([nr,nc,path+1])
+                    grid[nr][nc]=1
         return -1
+
+
+        
