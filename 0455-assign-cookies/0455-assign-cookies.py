902fa10098430b46aa28412383b53990ba1de628
khasimbi23 <shaikkhasimbi223@gmail.com>
2026-06-12 10:15:14 +0530

    Time: 25 ms (38.77%), Space: 22 MB (69.37%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        i=j=0
+        g.sort()
+        s.sort()
+        while i<len(g) and j<len(s):
+            if s[j]>=g[i]:
+                i+=1
+            j+=1
+        return i
