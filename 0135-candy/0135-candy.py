b5c1f0909d9aea49ff8ccbeadad1120800ec610c
khasimbi23 <shaikkhasimbi223@gmail.com>
2026-06-16 09:05:14 +0530

    Time: 19 ms (36.68%), Space: 21.5 MB (45.57%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -2,16 +2,18 @@ class Solution:
     def candy(self, ratings: List[int]) -> int:
         n=len(ratings)
         l=[1]*n
-        r=[1]*n
         for i in range(1,n):
             if ratings[i]>ratings[i-1]:
                 l[i]=l[i-1]+1
+        s=l[n-1]
+        cur=1
         for i in range(n-2,-1,-1):
             if ratings[i]>ratings[i+1]:
-                r[i]=r[i+1]+1
-        s=0
-        for i in range(n):
-            s=s+max(l[i],r[i])
+                cur+=1
+            else:
+                cur=1
+            s=s+max(l[i],cur)
         return s
-          
+#t.c=o(2n)
+#s.c=o(n)
         
