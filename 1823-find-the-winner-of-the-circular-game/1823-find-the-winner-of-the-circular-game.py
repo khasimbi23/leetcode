8aabf024a627ac446e0b43f73eaac9931921cd3a
khasimbi23 <shaikkhasimbi223@gmail.com>
2026-06-17 09:24:22 +0530

    Time: 0 ms (100%), Space: 19.2 MB (57.88%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,11 +1,8 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        # using queue data structure
-        d=deque(range(1,n+1))
-        while len(d)>1:
-            for i in range(k-1):
-                x=d.popleft()
-                d.append(x)
-            d.popleft()
-        return d[0]
-        
+        # using recursion
+        def helper(n,k):
+            if n==1:
+                return 0
+            return (helper(n-1,k)+k)%n
+        return 1+helper(n,k)
