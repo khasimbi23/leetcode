397ff805fb0bc31bd131c277d30a79e69f41c720
khasimbi23 <shaikkhasimbi223@gmail.com>
2026-06-19 09:02:34 +0530

    Time: 4 ms (10.2%), Space: 19.4 MB (65.98%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,19 +1,13 @@
-class Solution(object):
-    def permute(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        res = []
-        def backtrack(path, remaining):
-            if not remaining:
-                res.append(path)
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        def backtracking(i):
+            if i==len(nums):
+                res.append(nums[:])
                 return
-            for i in range(len(remaining)):
-                backtrack(path + [remaining[i]],
-                          remaining[:i] + remaining[i+1:])
-
-        backtrack([], nums)
+            for j in range(i,len(nums)):
+                nums[i],nums[j]=nums[j],nums[i]
+                backtracking(i+1)
+                nums[i],nums[j]=nums[j],nums[i]
+        res=[]
+        backtracking(0)
         return res
-            
-        
