7704724acea3aa242b72df8c539c8f9e7328d3cc
khasimbi23 <shaikkhasimbi223@gmail.com>
2026-06-25 10:39:16 +0530

    Time: 247 ms (66.46%), Space: 20.8 MB (87.46%) - LeetHub

---
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -0,0 +1,25 @@
+from heapq import *
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        m=len(heights)
+        n=len(heights[0])
+        grid=[[float('inf')]*n for i in range(m)]
+        grid[0][0]=0
+        minheap=[(0,0,0)] #diff,r,c
+        d=[[0,1],[1,0],[0,-1],[-1,0]]
+        while minheap:
+            diff,r,c = heapq.heappop(minheap)
+            if diff>grid[r][c]:
+                continue
+            for dr,dc in d:
+                nr,nc = dr+r,dc+c
+                if 0<=nr<m and 0<=nc<n:
+                    newdiff=max(abs(heights[nr][nc]-heights[r][c]),diff)
+                    if newdiff<grid[nr][nc]:
+                        grid[nr][nc]=newdiff
+                        heappush(minheap,(newdiff,nr,nc))
+        return grid[m-1][n-1]
+
+
+
+        
